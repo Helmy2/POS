11e96774bf753b6092681341c5d1672b2b08fd87
Helmy2 <mohelmy.eng@gmail.com>
2025-08-16 12:59:43 +0300

    feat: Add category icon and update InventoryScreen to use LazyVerticalGrid with Card items

--- app/src/commonMain/kotlin/com/wael/astimal/pos/features/inventory/presentation/inventory/InventoryScreen.kt
+++ app/src/commonMain/kotlin/com/wael/astimal/pos/features/inventory/presentation/inventory/InventoryScreen.kt
@@ -1,17 +1,27 @@
 package com.wael.astimal.pos.features.inventory.presentation.inventory
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.wael.astimal.pos.core.presentation.compoenents.ItemGrid
-import com.wael.astimal.pos.core.presentation.compoenents.Label
 import com.wael.astimal.pos.core.presentation.compoenents.Screen
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
+import pos.app.generated.resources.Res
+import pos.app.generated.resources.category
 
 @Composable
 fun InventoryRoute(
@@ -29,19 +39,31 @@ fun InventoryScreen(
     state: InventoryReducer.State,
     onEvent: (InventoryReducer.Event) -> Unit,
 ) {
-    Screen(
-        modifier = Modifier.padding(16.dp),
-    ) {
-        ItemGrid(
-            list = state.items,
-            onItemClick = { inventoryItem ->
-                onEvent(InventoryReducer.Event.ItemClicked(inventoryItem.destination))
-            },
-            label = { inventoryItem ->
-                Label(text = stringResource(inventoryItem.label))
-            },
-            modifier = Modifier.fillMaxSize(),
-            isSelected = { false }
-        )
+    Screen {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 150.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            items(state.items) {
+                Card(
+                    onClick = {
+                        onEvent(InventoryReducer.Event.ItemClicked(it.destination))
+                    },
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.category),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                    Text(
+                        text = stringResource(it.label),
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+            }
+        }
     }
 }
