ac3cc9fb4ba0c7f52e7b60e83e6edc2ba44b9d62
Helmy2 <mohelmy.eng@gmail.com>
2025-09-09 20:13:24 +0300

    Fix: Use purchase price for average price if not set

--- app/src/commonMain/kotlin/com/wael/astimal/pos/features/inventory/presentation/product/ProductViewModel.kt
+++ app/src/commonMain/kotlin/com/wael/astimal/pos/features/inventory/presentation/product/ProductViewModel.kt
@@ -130,7 +130,8 @@ class ProductViewModel(
                 val productToSave = Product(
                     id = currentState.selectedProduct?.product?.id ?: "",
                     name = LocalizedString(currentState.inputArName, currentState.inputEnName),
-                    averagePrice = currentState.selectedProduct?.product?.averagePrice ?: 0.0,
+                    averagePrice = currentState.selectedProduct?.product?.averagePrice.takeIf { it != 0.0 }
+                        ?: currentState.inputPurchasePrice.toDoubleOrNull() ?: 0.0,
                     sellingPrice = currentState.inputSellingPrice.toDoubleOrNull() ?: 0.0,
                     subUnitsPerMainUnit = currentState.inputSubUnitsPerMainUnit.toDoubleOrNull()
                         ?: 1.0,
